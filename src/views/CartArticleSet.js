b0ad7558b6232fea88c8ed74c1a0ac5df31f3f76
david-blanchard <dpjb@hotmail.fr>
2020-08-22 13:48:56 +0200

    Make a React standard cart display

--- src/views/CartArticleSet.js
+++ src/views/CartArticleSet.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import uuid from 'react-uuid'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import BusinessCart from '../business/Cart'
@@ -7,45 +7,50 @@ import BusinessCart from '../business/Cart'
 class CartArticleSet extends Component {
   constructor (props) {
     super(props)
+    this.handleRemove = this.handleRemove.bind(this)
     this.state = {
-      cart: BusinessCart.readCart()
+      cart: BusinessCart.readCart(),
+      lines: []
     }
   }
 
   componentDidMount () {
-    this.displayCart()
+    this.computeCart()
   }
 
-  displayCart () {
+  computeCart () {
     const businessCart = new BusinessCart()
-    const cart = this.state.cart
-    let total = 0
-    let i = 1
+    const cart = BusinessCart.readCart()
+    businessCart.computeDiscount = businessCart.computeDiscount.bind(this)
+    let subtotal = 0.0
 
-    cart.forEach((article) => {
+    const cartLines = cart.map((article, i) => {
       // Add an article to the cart
-      CartArticleSet.appendComponent(<ArticleLine article={article} index={i} parent={this} />, 'article' + i)
-      total += parseFloat(article.price)
-      i++
+      subtotal += parseFloat(article.price)
+
+      return { key: 'article', value: i }
     })
 
-    total = total.toFixed(2)
+    subtotal = subtotal.toFixed(2)
 
-    // Add the sub-total to the cart
-    CartArticleSet.appendComponent(<SubTotalLine sum={total} parent={this} />, 'subtotal')
+    cartLines.push({ key: 'subtotal', value: subtotal })
 
     // Computes the discount sum
-    businessCart.computeDiscount(total, (discountSum) => {
+    businessCart.computeDiscount(subtotal, (discountSum) => {
       discountSum = parseFloat(discountSum).toFixed(2)
 
-      // Add the discount sum line to the cart
-      CartArticleSet.appendComponent(<DiscountLine discountSum={discountSum} parent={this} />, 'discount')
+      cartLines.push({ key: 'discount', value: discountSum })
 
-      total -= discountSum
-      total = parseFloat(total).toFixed(2)
+      const total = parseFloat(subtotal - discountSum).toFixed(2)
 
-      // Add the total line to the cart
-      CartArticleSet.appendComponent(<TotalLine total={total} parent={this} />, 'total')
+      cartLines.push({ key: 'total', value: total })
+
+      const res = this.setState({
+        cart: cart,
+        lines: cartLines
+      })
+
+      return res
     })
   }
 
@@ -62,20 +67,35 @@ class CartArticleSet extends Component {
     const tableLines = document.querySelector('#table-lines')
     tableLines.innerHTML = ''
 
-    this.setState({ cart: BusinessCart.readCart() })
-
-    this.displayCart()
+    this.computeCart()
   }
 
-  // Append a line to the cart table
-  static appendComponent (element, id) {
-    const tableLines = document.querySelector('#table-lines')
+  handleRemove (keyid) {
+    // console.log(e.target.attributes)
+    const { text } = this.state
+    let reduced = []
+    let removed = ''
+    reduced = text.reduce((reduced, iteratee) => {
+      if (iteratee.key === keyid) {
+        removed = iteratee
+      }
+      if (iteratee.key !== keyid) {
+        reduced.push(iteratee)
+      }
 
-    const widget = document.createElement('tr')
-    widget.id = 'row-' + id
-    tableLines.appendChild(widget)
+      return reduced
+    }, reduced)
 
-    ReactDOM.render(element, widget)
+    console.log({ array: reduced })
+    console.log({ removed: removed.value })
+
+    const res = this.setState({
+      text: reduced
+    })
+
+    this.props.onModify(reduced)
+
+    return res
   }
 
   render () {
@@ -92,7 +112,39 @@ class CartArticleSet extends Component {
               <th> </th>
             </tr>
           </thead>
-          <tbody id='table-lines' />
+          <tbody id='table-lines'>
+            {
+              this.state.lines.map((line, i) => {
+                // Add an article to the cart
+
+                const keyid = uuid()
+                /* Add an article line to the table */
+                if (line.key === 'article') {
+                  const id = line.value
+                  const article = this.state.cart[id]
+                  // console.log({ id: id, article: article, cart: this.state.cart })
+
+                  return (<ArticleLine key={keyid} article={article} index={i} onRemove={this.handleRemove} />)
+                }
+
+                /* Add the sub-total line to the table */
+                if (line.key === 'subtotal') {
+                  const subtotal = line.value
+                  return (<SubTotalLine key={uuid()} sum={subtotal} />)
+                }
+                /* Add the discount line to the table */
+                if (line.key === 'discount') {
+                  const discount = line.value
+                  return (<DiscountLine key={uuid()} discountSum={discount} />)
+                }
+                /* Add the total line to the table */
+                if (line.key === 'total') {
+                  const total = line.value
+                  return (<TotalLine key={uuid()} total={total} />)
+                }
+              })
+            }
+          </tbody>
         </table>
       </div>
     )
@@ -105,7 +157,6 @@ class ArticleLine extends Component {
 
     const json = this.props.article
     this.index = this.props.index
-    this.parent = this.props.parent
 
     this.cover = json.cover
     this.price = parseFloat(json.price).toFixed(2)
@@ -115,7 +166,7 @@ class ArticleLine extends Component {
   // const isbn = json.isbn
   render () {
     return (
-      <>
+      <tr>
         <td><img src={this.cover} width='40' height='60' /> </td>
         <td>{this.title}</td>
         <td>En stock</td>
@@ -124,50 +175,53 @@ class ArticleLine extends Component {
         <td className='text-right'>
           <button onClick={(e) => { this.parent.removeFromCart(e) }} data-index={this.index} className='remove-from-cart-cta btn btn-sm btn-danger'>
             <FontAwesomeIcon pointerEvents='none' icon={faTrash} />
-
           </button>
         </td>
-      </>
+      </tr>
     )
   }
 }
 
 const SubTotalLine = ({ sum }) => {
   return (
-    <>
+    <tr>
       <td />
       <td />
       <td />
       <td>Sous-total</td>
       <td className='text-right'>{sum} €</td>
       <td />
-    </>
+    </tr>
   )
 }
 
 const DiscountLine = ({ discountSum }) => {
+  console.log({ discountSum: discountSum })
+
   return (
-    <>
+    <tr>
       <td />
       <td />
       <td />
       <td>Meilleure remise</td>
       <td className='text-right'>{discountSum} €</td>
       <td />
-    </>
+    </tr>
   )
 }
 
 const TotalLine = ({ total }) => {
+  console.log({ total: total })
+
   return (
-    <>
+    <tr>
       <td />
       <td />
       <td />
       <td><strong>Total TTC</strong></td>
       <td className='text-right'><strong>{total} €</strong></td>
       <td />
-    </>
+    </tr>
   )
 }
 export default CartArticleSet
